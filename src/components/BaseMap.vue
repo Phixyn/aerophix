8d9ec6ee5e5a52c63c38cc86b8268b0e05701006
phixyn <phixyn@gmail.com>
2021-04-02 22:36:17 +0100

    BaseMap: Implement a component to display a Mapbox GLJS map
    Implement BaseMap component. For now it displays a basic map.

---
+++ src/components/BaseMap.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="map" class="base-map"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default {
+  name: "BaseMap",
+  data() {
+    return {
+      accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN,
+    };
+  },
+  mounted() {
+    mapboxgl.accessToken = this.accessToken;
+
+    new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-98, 38.88],
+      maxZoom: 5,
+      minZoom: 1,
+      zoom: 3,
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#map {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <HelloWorld />
+    <BaseMap />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BaseMap from "@/components/BaseMap.vue";
 
 export default {
   name: "Home",
 
   components: {
-    HelloWorld,
+    BaseMap,
   },
 };
 </script>
