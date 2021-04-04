b990179f1c8d010b0c7a3423f9a9cd621d40ec53
phixyn <phixyn@gmail.com>
2021-04-04 21:53:35 +0100

    BaseMap: Display airport location icons on the map
    Add airports layer to the map.

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -18,13 +18,31 @@ export default {
   mounted() {
     mapboxgl.accessToken = this.accessToken;
 
-    new mapboxgl.Map({
+    const map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11",
       center: [-98, 38.88],
       maxZoom: 5,
       minZoom: 1,
       zoom: 3,
+    });
+
+    map.on("load", () => {
+      map.addSource("airports", {
+        "type": "vector",
+        "url": "mapbox://mapbox.04w69w5j",
+      });
+      map.addLayer({
+        "id": "airport",
+        "source": "airports",
+        "source-layer": "ne_10m_airports",
+        "type": "symbol",
+        "layout": {
+          "icon-image": "airport-15",
+          "icon-padding": 0,
+          "icon-allow-overlap": true,
+        },
+      });
     });
   },
 };
