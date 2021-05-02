8a766a62f6450c94c0576dc501a930370717275e
phixyn <phixyn@gmail.com>
2021-05-02 22:40:03 +0100

    AirportListItem: Display more information and an avatar
    Display ICAO, IATA, an avatar image and a "View" button in each list item.

--- src/components/AirportListItem.vue
+++ src/components/AirportListItem.vue
@@ -1,23 +1,30 @@
 <template>
-  <v-list-item>
-    <v-list-item-content>
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      >
-      </v-img>
-      <v-divider></v-divider>
+  <div class="list-item">
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title v-text="airport.properties['name']">
+        </v-list-item-title>
+        <v-list-item-subtitle v-text="airport.properties['iata_code']">
+        </v-list-item-subtitle>
 
-      <v-list-item-title v-text="airport.properties['gps_code']">
-      </v-list-item-title>
-      <v-list-item-subtitle v-text="airport.properties['gps_code']">
-      </v-list-item-subtitle>
-      <a v-bind:href="airport.properties.wikipedia" target="_blank">
-        Wikipedia
-      </a>
-    </v-list-item-content>
-  </v-list-item>
+        <p>
+          {{ airport.properties["gps_code"] }} |
+          <a v-bind:href="airport.properties.wikipedia" target="_blank">
+            Wikipedia
+          </a>
+        </p>
+
+        <p class="mt-6"><v-btn>View &rarr;</v-btn></p>
+      </v-list-item-content>
+
+      <v-list-item-avatar>
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"></v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-divider></v-divider>
+  </div>
+  <!-- .list-item -->
 </template>
 
 <script>
