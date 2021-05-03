01f849d732fb2436090a67fd478405c80c048b11
phixyn <phixyn@gmail.com>
2021-05-03 23:57:08 +0100

    App: Move header to a new component
    Move header to AppHeader component.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <v-app>
-    <!-- TODO put in a AppHeader component -->
-    <v-app-bar app dense color="primary" dark>
-
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-toolbar-title class="text-h4">AeroPhix</v-toolbar-title>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn text to="/">Home</v-btn>
-      <v-btn text to="/about">About</v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <AppHeader />
 
     <v-main>
       <!-- Provides the application the proper gutter -->
@@ -36,7 +12,10 @@
 </template>
 
 <script>
+import AppHeader from "@/components/AppHeader.vue";
+
 export default {
   name: "App",
+  components: { AppHeader },
 };
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar app dense color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-toolbar-title class="text-h4">AeroPhix</v-toolbar-title>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text to="/">Home</v-btn>
+    <v-btn text to="/about">About</v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
