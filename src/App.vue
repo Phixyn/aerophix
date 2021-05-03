11b01f506116d9bf848728322469fda13f7a9df5
phixyn <phixyn@gmail.com>
2021-05-03 23:04:56 +0100

    App: Remove unused code
    Remove search button from navbar and the data() which was unused (previously
    used for the nav drawer, which we removed). Also give the component a name.

--- src/App.vue
+++ src/App.vue
@@ -24,10 +24,6 @@
       <v-btn icon>
         <v-icon>mdi-heart</v-icon>
       </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -41,6 +37,6 @@
 
 <script>
 export default {
-  data: () => ({ showDrawer: true }),
+  name: "App",
 };
 </script>
