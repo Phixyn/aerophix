30a5d08ee9b276b405a01ada60215ca189f27291
phixyn <phixyn@gmail.com>
2021-05-08 22:30:13 +0100

    AirportInfoCard: Add a back button and update styling

--- src/components/AirportInfoCard.vue
+++ src/components/AirportInfoCard.vue
@@ -1,27 +1,38 @@
 <template>
-  <!-- TODO change max-width (or dont even set one lol) -->
-  <v-card class="mx-auto" max-width="400">
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+  <div>
+    <v-sheet class="sheet-header grey lighten-2 px-4 py-2">
+      <v-btn icon title="Back">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+    </v-sheet>
+
+    <v-card
+      class="mx-auto"
+      flat
+      rounded="0"
     >
-      <v-card-title>ICAO</v-card-title>
-    </v-img>
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title>ICAO</v-card-title>
+      </v-img>
 
-    <v-card-subtitle class="pb-0">IATA</v-card-subtitle>
+      <v-card-subtitle class="pb-0">IATA</v-card-subtitle>
 
-    <v-card-text class="text--primary">
-      <div>Airport Name</div>
+      <v-card-text class="text--primary">
+        <div>Airport Name</div>
 
-      <div>City, ST, USA</div>
-    </v-card-text>
+        <div>City, ST, USA</div>
+      </v-card-text>
 
-    <v-card-actions>
-      <v-btn color="orange" text>View</v-btn>
-      <v-btn color="orange" text>Explore</v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-card-actions>
+        <v-btn color="orange" text>View</v-btn>
+        <v-btn color="orange" text>Explore</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -29,3 +40,9 @@ export default {
   name: "AirportInfoCard",
 };
 </script>
+
+<style lang="scss" scoped>
+.sheet-header {
+  // TODO
+}
+</style>
